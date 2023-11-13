e92752386f9b5b8a1f4c6fbc400784342ea1d3dd
maria-inigo <mariainigo97@gmail.com>
2023-11-13 20:26:17 +0000

    refactors workflow

--- next-site/next.config.js
+++ next-site/next.config.js
@@ -2,20 +2,21 @@
  * @type {import('next').NextConfig}
  */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = ''
+// let assetPrefix = ''
+// let basePath = ''
 
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
 
 module.exports = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  distDir: 'dist',
+  // assetPrefix: assetPrefix,
+  // basePath: basePath,
 }
